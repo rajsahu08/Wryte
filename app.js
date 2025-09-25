4641d33c15e7b415c7a7bf2badcbe0b5f5166975
rajsahu08 <rajsahu0702@gmail.com>
2025-09-26 05:17:19 +0530

    Search funtionality implemented

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
-if(process.env.NODE_ENV !== "production"){
+if (process.env.NODE_ENV !== "production") {
     require("dotenv").config();
 }
 
 const express = require('express');
-const app=express();
+const app = express();
 const engine = require("ejs-mate");
 const Path = require("path");
 const mongoose = require("mongoose");
 const article = require("./models/article");
 const methodOverride = require("method-override");
 const session = require("express-session");
-const MongoStore = require("connect-mongo")
+const MongoStore = require("connect-mongo");
+const articleRouter = require("./routes/article");
 
-const articleRouter = require("./routes/article")
-
-app.engine('ejs',engine);
-app.set("view engine","ejs");
-app.set("views",Path.join(__dirname,"views"));
+app.engine('ejs', engine);
+app.set("view engine", "ejs");
+app.set("views", Path.join(__dirname, "views"));
 
 
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'));
 app.use(session({
     secret: process.env.SESSION_SECRET,
@@ -28,7 +27,7 @@ app.use(session({
     saveUninitialized: true,
     store: MongoStore.create({
         mongoUrl: process.env.ATLAS_DB_URL,
-    }), 
+    }),
     cookie: {
         httpOnly: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
@@ -37,26 +36,44 @@ app.use(session({
 }))
 
 //Connecting to MongoDB Atlas
-const MONGO_URL=process.env.ATLAS_DB_URL;
+const MONGO_URL = process.env.ATLAS_DB_URL;
 
-main().then(()=>{
+main().then(() => {
     console.log("Database Connected Successfully!");
-}).catch((err)=>{
+}).catch((err) => {
     console.log(err);
 })
 
-async function main(){
+async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
 //Home Route
-app.get("/",async(req,res)=>{
+app.get("/", async (req, res) => {
     const articles = await article.find({});
-    res.render("./articles/index.ejs",{articles});
+    res.render("./articles/index.ejs", { articles, query: "" });
 })
+// Add this below your existing routes in app.js
+app.get("/search", async (req, res) => {
+    const { query } = req.query; // Get the search query from the URL
+    if (!query) {
+        // If no query is provided, render the index with all articles or an empty result
+        const articles = await article.find({});
+        return res.render("./articles/index.ejs", { articles, query: "" });
+    }
 
-app.use("/articles",articleRouter);
+    // Search for articles where title or description contains the query (case-insensitive)
+    const articles = await article.find({
+        $or: [
+            { title: { $regex: query, $options: "i" } },
+            { description: { $regex: query, $options: "i" } }
+        ]
+    });
 
-app.listen(3000,()=>{
+    res.render("./articles/index.ejs", { articles, query });
+});
+app.use("/articles", articleRouter);
+
+app.listen(3000, '0.0.0.0', () => {
     console.log("App is listening at port 3000");
 })
